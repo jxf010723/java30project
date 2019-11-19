bf1ef1f2e47195338131e3414449b81ac13bb43a
1076464015 <1076464015@qq.com>
2019-11-19 08:22:54 +0800

    FJ1

--- Money/src/main/java/com/accp/service/GoodsService.java
+++ Money/src/main/java/com/accp/service/GoodsService.java
@@ -1,5 +1,64 @@
 package com.accp.service;
 
-public class GoodsService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.accp.domain.Goodstype;
+import com.accp.domain.Shop;
+import com.accp.mapper.GoodstypeMapper;
+import com.accp.mapper.ShopMapper;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+@Service
+@Transactional
+public class GoodsService {
+	@Autowired
+	GoodstypeMapper goodstype;
+	@Autowired
+	ShopMapper shop;
+	
+	/**
+	 * 商品类型查询分页
+	 * @param pageNum
+	 * @return
+	 */
+	public PageInfo<Goodstype> goodstypepage(int pageNum){
+		PageHelper.startPage(pageNum, 5);
+		List<Goodstype> list=goodstype.selectByExample(null);
+		PageInfo<Goodstype> page=new PageInfo<Goodstype>(list);
+		return page;
+	}
+	/**
+	 * 商品类型新增
+	 * @param typename
+	 * @return
+	 */
+//	public int goodstypeinsert(String typename) {
+//		return goodstype.insert(typename);
+//	}
+	/**
+	 * 商品类型修改
+	 * @param name
+	 * @param id
+	 * @return
+	 */
+//	public int goodstypeupdate(String name,String id) {
+//		return goodstype.updateByPrimaryKey(name, id);
+//	}
+	/**
+	 * 商品类型删除
+	 * @param typeid
+	 * @return
+	 */
+	public int goodstypedelete(Integer typeid) {
+		return goodstype.deleteByPrimaryKey(typeid);
+	}
+	
+	public List<Shop> queryshop(){
+		return shop.selectByExample(null);
+	}
 }
