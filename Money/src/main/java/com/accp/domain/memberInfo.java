5b0e790c29e9d7fc2f45f9b15a7ec08988b6670f
jxf010723 <3070526392@qq.com>
2019-11-28 14:46:06 +0800

    master2019-11-28第一次提交

---
+++ Money/src/main/java/com/accp/domain/memberInfo.java
@@ -0,0 +1,156 @@
+package com.accp.domain;
+
+public class memberInfo {
+	
+	private Integer vipId;
+	
+	private Integer userId;
+	
+	private String vipName;
+	
+	private String vipPwd;
+	
+	private String vipProvince;
+
+    private String vipCity;
+
+    private String vipRegion;
+    
+    private String vipStreet;
+	
+	private String vipPhone;
+	
+	private String viptypeName;
+	
+	private Integer vipTurnover;
+	
+	private Double balance;
+	
+	private Integer integral;
+	
+	private Integer zjcj;
+	
+	private double price;
+	
+	public String getVipStreet() {
+		return vipStreet;
+	}
+
+	public void setVipStreet(String vipStreet) {
+		this.vipStreet = vipStreet;
+	}
+
+	public String getVipPwd() {
+		return vipPwd;
+	}
+
+	public void setVipPwd(String vipPwd) {
+		this.vipPwd = vipPwd;
+	}
+
+	public String getVipProvince() {
+		return vipProvince;
+	}
+
+	public void setVipProvince(String vipProvince) {
+		this.vipProvince = vipProvince;
+	}
+
+	public String getVipCity() {
+		return vipCity;
+	}
+
+	public void setVipCity(String vipCity) {
+		this.vipCity = vipCity;
+	}
+
+	public String getVipRegion() {
+		return vipRegion;
+	}
+
+	public void setVipRegion(String vipRegion) {
+		this.vipRegion = vipRegion;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Integer getVipId() {
+		return vipId;
+	}
+
+	public void setVipId(Integer vipId) {
+		this.vipId = vipId;
+	}
+
+	public String getVipName() {
+		return vipName;
+	}
+
+	public void setVipName(String vipName) {
+		this.vipName = vipName;
+	}
+
+	public String getVipPhone() {
+		return vipPhone;
+	}
+
+	public void setVipPhone(String vipPhone) {
+		this.vipPhone = vipPhone;
+	}
+
+	public String getViptypeName() {
+		return viptypeName;
+	}
+
+	public void setViptypeName(String viptypeName) {
+		this.viptypeName = viptypeName;
+	}
+
+	public Integer getVipTurnover() {
+		return vipTurnover;
+	}
+
+	public void setVipTurnover(Integer vipTurnover) {
+		this.vipTurnover = vipTurnover;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Integer getIntegral() {
+		return integral;
+	}
+
+	public void setIntegral(Integer integral) {
+		this.integral = integral;
+	}
+
+	public Integer getZjcj() {
+		return zjcj;
+	}
+
+	public void setZjcj(Integer zjcj) {
+		this.zjcj = zjcj;
+	}
+	
+	
+}
